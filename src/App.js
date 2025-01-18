79e63ea471cc3f396826671f72cfdb58380338c9
Soukaina-AK <soukainaakaroum05@gmail.com>
2025-01-18 20:50:59 +0100

    Authentication branch creation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<div>main</div>} />
-        <Route path='/login' element={<div>login</div>} />
+        <Route path='/login' element={<Login/>} />
         <Route path='/register' element={<div>register</div>} />
       </Routes>
       
