106c771c202ad8e8b521060f311605b5eb08b6b2
Ali-404 <abdellali.aitaddi@gmail.com>
2025-01-19 00:25:23 +0100

    fix resp, fix btn

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,12 +5,13 @@ import LockIcon from '@mui/icons-material/Lock';
 import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
 import GoogleIcon from '@mui/icons-material/Google';
 import AppleIcon from '@mui/icons-material/Apple';
+import { Button } from '@mui/material';
 
 
 export default function Login() {
   return (
     <div className='min-h-screen bg-gradient-to-tl from-light_cyan-600 to-light_cyan-900 flex items-center justify-center'>
-        <div className='bg-gradient-to-b to-white-500 from-light_cyan-800 w-[22vw] h-[50vh] border border-gray-300 rounded-xl drop-shadow flex flex-col items-center justify-center gap-5 px-6'>
+        <div className='bg-gradient-to-b to-white-500 from-light_cyan-800 w-[450px] border border-gray-300 rounded-xl drop-shadow flex flex-col items-center justify-center gap-5 px-6'>
             <div className='w-14 h-14 bg-white shadow rounded-2xl flex items-center justify-center'><LoginIcon/></div>
             <div className='flex flex-col items-center justify-center gap-1'>
                 <h1 className='text-2xl font-bold'>Sign in with email</h1>
@@ -28,16 +29,16 @@ export default function Login() {
                 <p className='self-end text-sm'>Forgot password?</p>
             </div>
             <div className='flex flex-col items-center justify-center gap-3 w-full px-5'>
-                <button className='bg-black text-white w-full h-10 rounded-lg'>Get Started</button>
+                <Button variant='contained' color='error' sx={{backgroundColor:"black", color:"white"}} className='w-full h-10 rounded-lg'>Get Started</Button>
                 <div className='flex flex-row items-center justify-center gap-2'>
                     <hr className='border border-gray-300 w-28'/>
                     <small className='text-gray-500'>Or sign in with</small>
                     <hr className='border border-gray-300 w-28'/>
                 </div>
-                <div className='flex flex-row items-center justify-between w-full px-2'>
-                    <button className='bg-white h-10 w-24 shadow drop-shadow-sm rounded-lg'><GoogleIcon/></button>
-                    <button className='bg-white h-10 w-24 shadow drop-shadow-sm rounded-lg'><FacebookRoundedIcon/></button>
-                    <button className='bg-white h-10 w-24 shadow drop-shadow-sm rounded-lg'><AppleIcon/></button>
+                <div className='flex flex-row items-center justify-between w-full p-2'>
+                    <Button color='inherit' className='bg-white h-10 w-24 shadow drop-shadow-sm rounded-lg'><GoogleIcon/></Button>
+                    <Button color='inherit' className='bg-white h-10 w-24 shadow drop-shadow-sm rounded-lg'><FacebookRoundedIcon/></Button>
+                    <Button color='inherit' className='bg-white h-10 w-24 shadow drop-shadow-sm rounded-lg'><AppleIcon/></Button>
                 </div>
             </div>
         </div>
