b80355fd8b88e260111ec1ad26f0950bf1e08709
Soukaina-AK <soukainaakaroum05@gmail.com>
2025-01-19 00:29:57 +0100

    Fixed height of container - login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,7 +11,7 @@ import { Button } from '@mui/material';
 export default function Login() {
   return (
     <div className='min-h-screen bg-gradient-to-tl from-light_cyan-600 to-light_cyan-900 flex items-center justify-center'>
-        <div className='bg-gradient-to-b to-white-500 from-light_cyan-800 w-[450px] border border-gray-300 rounded-xl drop-shadow flex flex-col items-center justify-center gap-5 px-6'>
+        <div className='bg-gradient-to-b to-white-500 from-light_cyan-800 w-[450px] border border-gray-300 rounded-xl drop-shadow flex flex-col items-center justify-center gap-5 p-6'>
             <div className='w-14 h-14 bg-white shadow rounded-2xl flex items-center justify-center'><LoginIcon/></div>
             <div className='flex flex-col items-center justify-center gap-1'>
                 <h1 className='text-2xl font-bold'>Sign in with email</h1>
