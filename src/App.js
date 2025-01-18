e9a6c509c8e483ccd27a35819919429e7da55fad
Soukaina-AK <soukainaakaroum05@gmail.com>
2025-01-19 00:14:13 +0100

    C:/Program Files/Git/login route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Dashboard from './dashboard/Dashboard'
+import Login from './pages/Login'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Dashboard/>} />
-        <Route path='/login' element={<div>login</div>} />
-        <Route path='/register' element={<div>register</div>} />
+        <Route path='/login' element={<Login/>} />
       </Routes>
       
     </BrowserRouter>
